539cf56d14c94a482300766e2af2fd2d96963dca
abliveira <dev.ab@outlook.com>
2024-01-05 23:28:32 -0300

    Refactor WeatherData

--- app/src/main/java/com/abliveira/weatherapp/data/WeatherData.java
+++ app/src/main/java/com/abliveira/weatherapp/data/WeatherData.java
@@ -4,6 +4,7 @@ import org.json.JSONObject;
 
 public class WeatherData {
 
+    // Declare the variables.
     private String currTemp;
     private String maxTemp;
     private String minTemp;
@@ -12,79 +13,90 @@ public class WeatherData {
     private String weatherDescription;
     private String city;
 
+    // Singleton holder.
     private static class SingletonHolder {
         private static final WeatherData INSTANCE = new WeatherData();
     }
 
+    // Get the singleton instance.
     public static WeatherData getInstance() {
         return SingletonHolder.INSTANCE;
     }
 
-    public String getCurrTemp() {
-        return currTemp;
-    }
+    // Setters.
 
     public void setCurrTemp(String currTemp) {
         this.currTemp = currTemp;
-    }
-
-    public String getMaxTemp() {
-        return maxTemp;
     }
 
     public void setMaxTemp(String maxTemp) {
         this.maxTemp = maxTemp;
     }
 
-    public String getMinTemp() {
-        return minTemp;
-    }
-
     public void setMinTemp(String minTemp) {
         this.minTemp = minTemp;
-    }
-
-    public String getHumidity() {
-        return humidity;
     }
 
     public void setHumidity(String humidity) {
         this.humidity = humidity;
     }
 
-    public String getWindSpeed() {
-        return windSpeed;
-    }
-
     public void setWindSpeed(String windSpeed) {
         this.windSpeed = windSpeed;
-    }
-
-    public String getWeatherDescription() {
-        return weatherDescription;
     }
 
     public void setWeatherDescription(String weatherDescription) {
         this.weatherDescription = weatherDescription;
     }
 
-    public String getCity() {
-        return city;
-    }
-
     public void setCity(String city) {
         this.city = city;
     }
 
+    // Getters.
 
-    public void parseWeather(String s) {
+    public String getCurrTemp() {
+        return currTemp;
+    }
 
+    public String getMaxTemp() {
+        return maxTemp;
+    }
+
+    public String getMinTemp() {
+        return minTemp;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    // Parse the weather data.
+    public boolean parseWeather(String s) {
+
+        // Try to parse the data.
         try {
-
+            // Create a JSONObject from the string.
             JSONObject jsonObject = new JSONObject(s);
+
+            // Get the weather object.
             JSONObject weather = jsonObject.getJSONArray("weather").getJSONObject(0);
             JSONObject main = jsonObject.getJSONObject("main");
             JSONObject wind = jsonObject.getJSONObject("wind");
+
+            // Set the weather data.
             currTemp = main.getString("temp") + " °C";
             minTemp = main.getString("temp_min") + " °C";
             maxTemp = main.getString("temp_max") + " °C";
@@ -92,9 +104,12 @@ public class WeatherData {
             windSpeed = wind.getString("speed") + " m/s";
             weatherDescription = weather.getString("description");
             city = jsonObject.getString("name");
+            return true;
 
         } catch (Exception e){
+            // Print the stack trace if an exception occurs.
             e.printStackTrace();
+            return false;
         }
     }
 }
--- app/src/main/java/com/abliveira/weatherapp/data/WeatherDataFetchTask.java
+++ app/src/main/java/com/abliveira/weatherapp/data/WeatherDataFetchTask.java
@@ -2,19 +2,24 @@ package com.abliveira.weatherapp.data;
 
 import android.os.AsyncTask;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class WeatherDataFetchTask extends AsyncTask<String, Void, String> {
 
     private Callback callback;
 
+    // Callback interface
     public interface Callback {
         void onDownloadComplete();
     }
 
+    // Setter for the callback
     public void setCallback(Callback callback) {
         this.callback = callback;
     }
@@ -22,26 +27,76 @@ public class WeatherDataFetchTask extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... urls) {
 
-        String result = "";
-        URL url;
-        HttpURLConnection connection = null;
+        // This method is called on a background thread, so we can do
+        // network operations here.
 
+        // Create a string to store the result of the HTTP request.
+        String result = "";
+
+        // Create a URL object for the specified URL.
+        URL url;
         try {
             url = new URL(urls[0]);
-            connection = (HttpURLConnection) url.openConnection();
-            InputStream inputStream = connection.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            int data = inputStreamReader.read();
-            while (data != -1){
-                char current = (char) data;
-                result += current;
-                data = inputStreamReader.read();
-            } return result;
-
-        } catch (Exception e) {
+        } catch (MalformedURLException e) {
             e.printStackTrace();
             return null;
         }
+
+        // Create a HttpURLConnection object to connect to the URL.
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // Set the request method to GET.
+        try {
+            connection.setRequestMethod("GET");
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Connect to the URL.
+        try {
+            connection.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // Get the input stream from the connection.
+        InputStream inputStream;
+        try {
+            inputStream = connection.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // Create an InputStreamReader object to read the input stream.
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+        // Create a char to store the current character being read.
+        int data;
+
+        // Read the input stream character by character.
+        try {
+            while ((data = inputStreamReader.read()) != -1) {
+                // Convert the character to a string.
+                char current = (char) data;
+
+                // Add the string to the result string.
+                result += current;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // Return the result string.
+        return result;
     }
 
     @Override
@@ -49,13 +104,14 @@ public class WeatherDataFetchTask extends AsyncTask<String, Void, String> {
 
         super.onPostExecute(s);
 
+        // Get the WeatherData instance
         WeatherData weatherData = WeatherData.getInstance();
 
         // Accessing methods or properties
-        weatherData.parseWeather(s);
+        boolean ret = weatherData.parseWeather(s);
 
         // Notify the callback that the download is complete
-        if (callback != null) {
+        if (ret & callback != null) {
             callback.onDownloadComplete();
         }
     }
